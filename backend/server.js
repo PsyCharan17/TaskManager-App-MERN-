ba6424731a5a4ec01c02dd6a557eed363b7a3753
PsyCharan17 <gsaicharan152020@gmail.com>
2022-11-23 07:58:17 +0530

    Latest as of 22112022

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 dotenv.config();
 connectDB();
 app.use(express.json());
--- frontend/src/screens/LoginScreeen/LoginScreen.js
+++ frontend/src/screens/LoginScreeen/LoginScreen.js
@@ -1,25 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import MainScreen from "../../components/MainScreen";
 import "./LoginScreen.css";
+import axios from "axios";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      setLoading(true);
+      const { data } = await axios.post(
+        "api/users/login",
+        {
+          email,
+          password,
+        },
+        config
+      );
+
+      console.log(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+
   return (
     <MainScreen title="Login">
       <div className="loginContainer">
-        <Form>
+        <Form onSubmit={submitHandler}>
           <Form.Group
             controlId="formBasicEmail"
             style={{ marginBottom: "10px" }}
           >
             <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email" placeholder="Enter Email"></Form.Control>
+            <Form.Control
+              type="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password"></Form.Control>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
           </Form.Group>
           <Button variant="primary" type="submit" style={{ marginTop: "20px" }}>
             Submit
